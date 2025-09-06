aa75a121a5becc8464b88d7987b6b0b75d5264a0
Dakota Michael <dakotamichael@Dakotas-MacBook-Air.local>
2025-09-06 00:35:52 -0400

    Adds error handling to service and controller

--- app/Services/OrderReportService.php
+++ app/Services/OrderReportService.php
@@ -1,37 +1,39 @@
 <?php
 
 namespace App\Services;
+use Illuminate\Support\Facades\Log;
 
-class OrderReportService
-{
+class OrderReportService {
     protected $categoryModel;
     protected $orderModel;
 
-    public function __construct($categoryModel, $orderModel)
-    {
+    public function __construct($categoryModel, $orderModel) {
         $this->categoryModel = $categoryModel;
         $this->orderModel = $orderModel;
     }
     
     public function getCategorizedComments(): array {
-        // TODO: Add caching layer if performance becomes an issue
-        // TODO: Add error handling
-        $categorizedComments = [];
+        try {
+            $categorizedComments = [];
 
-        $categories = $this->categoryModel->all();
-        foreach ($categories as $category) {
-            $categorizedComments[$category->name] = $this->orderModel
+            $categories = $this->categoryModel->all();
+            foreach ($categories as $category) {
+                $categorizedComments[$category->name] = $this->orderModel
+                    ->newQuery()
+                    ->categorize($category->search_regexp)
+                    ->pluck('comments');
+            }
+
+            $searchRegexps = $categories->pluck('search_regexp')->all();
+            $categorizedComments['Miscellaneous Comments'] = $this->orderModel
                 ->newQuery()
-                ->categorize($category->search_regexp)
+                ->everythingElse($searchRegexps)
                 ->pluck('comments');
+
+            return $categorizedComments;
+        } catch (\Throwable $e) {
+            Log::error('Error in getCategorizedComments: ' . $e->getMessage());
+            return [];
         }
-
-        $searchRegexps = $categories->pluck('search_regexp')->all();
-        $categorizedComments['Miscellaneous Comments'] = $this->orderModel
-            ->newQuery()
-            ->everythingElse($searchRegexps)
-            ->pluck('comments');
-
-        return $categorizedComments;
     }
 }
