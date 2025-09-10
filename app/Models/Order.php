7cd73fa2d186fd1a62195c42943c9bcc15fc1313
Dakota Michael <dakotamichael@Dakotas-MacBook-Air.local>
2025-09-10 18:06:21 -0400

    Refactors n+1 query (DB scan/cat); caches cats

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -12,22 +12,4 @@ class Order extends Model
     protected $keyType = 'string';
     public $incrementing = false;
     public $timestamps = false;
-
-    // Categorizes comments based on category search regexp: stored in categories table
-    public function scopeCategorize($query, $regexp = null){
-        if ($regexp && is_string($regexp)) {
-            return $query->whereRaw('comments REGEXP ?', [$regexp]);
-        }
-        return $query;
-    }
-
-    // Gets everything else nto matching the regexps in categories
-    public function scopeEverythingElse($query, $excludedRegexps = null){
-        if ($excludedRegexps && is_array($excludedRegexps)) {
-            foreach ($excludedRegexps as $regexp) {
-                $query->whereRaw('NOT (comments REGEXP ?)', [$regexp]);
-            }
-        }
-        return $query;
-    }
 }
